6f498db9c834df13e74bc119d375770c35ef90bb
Alex Wong <alex-ptwong@vgt-hk.net>
2020-09-01 16:18:08 +0800

    updated

--- game/object/ShieldBall.js
+++ game/object/ShieldBall.js
@@ -17,6 +17,7 @@ module.exports = class ShieldBall extends GameObject {
     collide(otherObject) {
         super.collide(otherObject);
         if (this.removed && typeof otherObject.setWeapon === 'function') {
+            if (otherObject.shield < 0) otherObject.shield = 0;
             otherObject.shield += 10;
         } else {
             this.removed = false;
